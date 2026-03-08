68310453baa4da94cbc00cf6edff9a116ee21cda
natesanshreyas <53503760+natesanshreyas@users.noreply.github.com>
2026-03-08 16:35:17 -0400

    feat: provision resources for RITM0010063

---
+++ provisioned/RITM0010063/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus2"
+}
