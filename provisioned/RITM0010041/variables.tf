32d6410e43c87838a903901e11dc2fd8d53c411b
natesanshreyas <53503760+natesanshreyas@users.noreply.github.com>
2026-03-07 19:48:35 -0500

    feat: provision resources for RITM0010041

---
+++ provisioned/RITM0010041/variables.tf
@@ -0,0 +1,11 @@
+variable "cost_center" {
+  description = "Cost center for billing"
+  type        = string
+  default     = "CC-UNKNOWN"
+}
+
+variable "ticket_id" {
+  description = "ServiceNow ticket ID"
+  type        = string
+  default     = "RITM0010041"
+}
